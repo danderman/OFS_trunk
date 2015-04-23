c3104ecf528b6cef2e66ae429e30bcebda12c659
Daryl Anderman <daryl@273kelvin.ca>
2015-04-23 08:20:26 -0400

    fix popup_src and popup_html to work in webkit browsers

--- public_html/shop/javascript.js
+++ public_html/shop/javascript.js
@@ -4,7 +4,9 @@
 // http://www.ericmmartin.com/projects/simplemodal/
 // Popup the simplemodal dialog for selecting membership renewal type
 
-function popup_html(html, modalID = "", closeTarget = "") {
+function popup_html(html, modalID, closeTarget) {
+  if (typeof modalID === 'undefined') { modalID = ''; }
+  if (typeof closeTarget === 'undefined') { closeTarget = ''; }
   // In the next line, the class = modalClose is the normal way to close the modal window but we will
   // replace that with an explicit call to closeTarget, if it is specified.
   jQuery.modal('<a class="modalCloseImg'+(closeTarget.length == 0 ? ' modalClose"' : '" onclick="window.location=\''+closeTarget+'\'"')+'>&nbsp;</a>'+html, {
@@ -18,6 +20,8 @@ function popup_html(html, modalID = "", closeTarget = "") {
   };
 
 // This is just a special case of popup_html()
-function popup_src(src, modalID = "", closeTarget = "") {
-  popup_html('<iframe src="'+src+'" id="simplemodal-iframe-'+modalID+'">', modalID = modalID, closeTarget = closeTarget);
+function popup_src(src, modalID, closeTarget) {
+  if (typeof modalID === 'undefined') { modalID = ''; }
+  if (typeof closeTarget === 'undefined') { closeTarget = ''; }
+ popup_html('<iframe src="'+src+'" id="simplemodal-iframe-'+modalID+'">', modalID = modalID, closeTarget = closeTarget);
   }
